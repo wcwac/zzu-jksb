90ff48ec15ea58eaee3dea0c3cfd614bef555e74
wcwac <16619878+wcwac@users.noreply.github.com>
2021-01-30 12:08:40 +0800

    Update jksb.py

--- jksb.py
+++ jksb.py
@@ -41,7 +41,7 @@ class Log:
         self.wd.set_window_size(600, 800)
         if self.loc:
             self.wd.execute_cdp_cmd("Page.setGeolocationOverride", {
-                                    "latitude": int(self.loc[0]), "longitude": float(self.loc[1]), "accuracy": 1})
+                                    "latitude": float(self.loc[0]), "longitude": float(self.loc[1]), "accuracy": 1})
 
     def loginProcesser(self) -> bool:
         self.wd.get('http://jksb.zzu.edu.cn/')
